d92e1b6dc3856795758c4e2bb92c79c53873cf5a
sabin3230 <saveen.man3230@gmail.com>
2022-11-02 15:24:03 +0545

    config

---
+++ config/blog.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Blog Website Moduel Configs
+    |--------------------------------------------------------------------------
+    |
+    | This is the file where you can store your blog site related configs
+    |
+    */
+
+    'comments' => [
+        'enabled' => true,
+    ],
+
+    'post' => [
+        'enabled' => true
+    ],
+
+    'tag' => [
+        'enabled' => true
+    ],
+
+    'user' => [
+        'enabled' => true
+    ],
+
+    'admin' => [
+        'enabled' => true
+    ],
+];
---
+++ config/constant.php
@@ -0,0 +1,9 @@
+<?
+return [
+    'status' => [
+        '1' => 'active',
+        '2' => 'inactive',
+        '3' => 'away',
+        '4' => 'DND',
+    ] 
+];
